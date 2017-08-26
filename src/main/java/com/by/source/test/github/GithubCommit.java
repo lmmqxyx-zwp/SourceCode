c22606531a464e03dc52d282a9d4108d52369a24
byvai <byvaio@outlook.com>
2017-08-26 23:19:52 +0800

    用idea提交

---
+++ src/main/java/com/by/source/test/github/GithubCommit.java
@@ -0,0 +1,15 @@
+package com.by.source.test.github;
+
+/**
+ * Created by byvai on 2017/8/26.
+ *
+ * 测试提交到github 用sourcetree软件
+ * @author zwp
+ */
+public class GithubCommit {
+
+    public static void main(String[] args) {
+        System.out.println("hello github sourcetree!");
+    }
+
+}
